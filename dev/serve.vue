bf91d4dce7d0d2238450199a0ca418ccaa5cbe4e
HEL Mab <mabhelitc@gmail.com>
2021-07-25 06:40:24 +0700

    enabled sfc-init boilerplate

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VuejsLoadingScreen from '@/vuejs-loading-screen.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VuejsLoadingScreen
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuejs-loading-screen />
+  </div>
+</template>
