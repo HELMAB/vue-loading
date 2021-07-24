76cf8102f2608fcc123011dc37fb074d716f3a70
HEL Mab <mabhelitc@gmail.com>
2021-07-25 06:55:08 +0700

    enabled sfc-init boilderplate

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VuejsLoadingScreenSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  VuejsLoadingScreenSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuejs-loading-screen-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VuejsLoadingScreenSample } from './vuejs-loading-screen-sample.vue';
